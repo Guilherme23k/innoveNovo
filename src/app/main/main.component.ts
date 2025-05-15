f131fc81dc0a9380482986f31c60e0d7f1821c86
Guilherme <guilhermeggr23@gmail.com>
2025-05-15 18:34:05 -0300

    feat: Adiciona o Carrousel no main component pelo TS

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CarrouselComponent } from "../carrousel/carrousel.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CarrouselComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
