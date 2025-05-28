6f3a99d335e67a7f4451d8c6c552c8dee87383fc
Guilherme <guilhermeggr23@gmail.com>
2025-05-28 20:25:57 -0300

    feat: adiciona componente do missao visao e valores

---
+++ src/app/msv/msv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-msv',
+  standalone: true,
+  imports: [],
+  templateUrl: './msv.component.html',
+  styleUrl: './msv.component.scss'
+})
+export class MSVComponent {
+
+}
