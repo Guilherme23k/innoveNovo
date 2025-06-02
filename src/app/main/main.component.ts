4844075560040d4585d1f25c4b33b01f6ae693fb
Guilherme <guilhermeggr23@gmail.com>
2025-06-02 20:35:22 -0300

    feat: Adiciona o msv no main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarrouselComponent } from "../carrousel/carrousel.component";
+import { MSVComponent } from "../msv/msv.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CarrouselComponent],
+  imports: [CarrouselComponent, MSVComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
