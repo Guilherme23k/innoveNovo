14c53a94c4df8ae293adaad99f0a6b0030a913d4
Guilherme <guilhermeggr23@gmail.com>
2025-06-04 19:22:14 -0300

    feat: adciona as imagems nos objetos da lista de items

--- src/app/msv/msv.component.ts
+++ src/app/msv/msv.component.ts
@@ -21,9 +21,9 @@ export class MSVComponent {
   
  lista: Item[] = [
 
-  {titulo: 'Missão', texto: 'Criando experiencias transformadoras,promovendo educação e desenvolvimento pessoal,mudando perspectivas.'},
-  {titulo: 'Visão', texto: 'Queremos ser refêrencia na área de eventos,ganhando reconhecimento pela inovação e transformação na vida de jovens.'},
-  {titulo: 'Valores', texto: 'São baseados em :Aprendizado contínuo;Impacto positivo;Comunicação assertiva;Inovação.'}
+  {titulo: 'Missão', texto: 'Criando experiencias transformadoras,promovendo educação e desenvolvimento pessoal,mudando perspectivas.', img: '/bandeira1.svg'},
+  {titulo: 'Visão', texto: 'Queremos ser refêrencia na área de eventos,ganhando reconhecimento pela inovação e transformação na vida de jovens.', img: '/badge.svg'},
+  {titulo: 'Valores', texto: 'São baseados em :Aprendizado contínuo;Impacto positivo;Comunicação assertiva;Inovação.', img: '/love.svg'}
   ];
 
 }
