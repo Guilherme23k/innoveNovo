dbd36f1d1a3ab972a43ed319db390c036cce6a1a
Guilherme <guilhermeggr23@gmail.com>
2025-06-04 19:39:26 -0300

    fix: resume um pouco os textos do card

--- src/app/msv/msv.component.ts
+++ src/app/msv/msv.component.ts
@@ -21,9 +21,9 @@ export class MSVComponent {
   
  lista: Item[] = [
 
-  {titulo: 'Missão', texto: 'Criando experiencias transformadoras,promovendo educação e desenvolvimento pessoal,mudando perspectivas.', img: '/bandeira1.svg'},
-  {titulo: 'Visão', texto: 'Queremos ser refêrencia na área de eventos,ganhando reconhecimento pela inovação e transformação na vida de jovens.', img: '/badge.svg'},
-  {titulo: 'Valores', texto: 'São baseados em :Aprendizado contínuo;Impacto positivo;Comunicação assertiva;Inovação.', img: '/love.svg'}
+  {titulo: 'Missão', texto: 'Transformar vidas com educação e experiências que inspiram crescimento e novas perspectivas.', img: '/bandeira1.svg'},
+  {titulo: 'Visão', texto: 'Ser referência em eventos que impactam jovens com inovação e aprendizado significativo.', img: '/badge.svg'},
+  {titulo: 'Valores', texto: 'Prezamos por inovação, impacto positivo, empatia, aprendizado contínuo e comunicação clara.', img: '/love.svg'}
   ];
 
 }
