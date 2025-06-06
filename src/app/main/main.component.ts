15cd02a8f4273ae26a6fc85d9bf1a49284bd649d
Guilherme <guilhermeggr23@gmail.com>
2025-06-05 21:24:57 -0300

    fix: adiciona o footerComponent no TS do main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CarrouselComponent } from "../carrousel/carrousel.component";
 import { MSVComponent } from "../msv/msv.component";
-
+import { FooterComponent } from "../footer/footer.component";
+  
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CarrouselComponent, MSVComponent],
+  imports: [CarrouselComponent, MSVComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
