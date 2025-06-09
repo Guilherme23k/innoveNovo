f9a2d4facfd5ed3929db84f46a12e16b5a0bcac6
Guilherme <guilhermeggr23@gmail.com>
2025-06-09 19:49:09 -0300

    feat: adiciona imports no mainTS que serão usados depois

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, HostBinding } from '@angular/core';
 import { CarrouselComponent } from "../carrousel/carrousel.component";
 import { MSVComponent } from "../msv/msv.component";
 import { FooterComponent } from "../footer/footer.component";
-  
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-main',
   standalone: true,
