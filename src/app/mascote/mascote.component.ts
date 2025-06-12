da7f759c550815c44340bee74a872f0bc795f482
Guilherme <guilhermeggr23@gmail.com>
2025-06-12 18:26:11 -0300

    feat: adiciona mascote component no projeto

---
+++ src/app/mascote/mascote.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascote',
+  standalone: true,
+  imports: [],
+  templateUrl: './mascote.component.html',
+  styleUrl: './mascote.component.scss'
+})
+export class MascoteComponent {
+
+}
