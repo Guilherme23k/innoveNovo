1a47bdf6c4fbfb06eb868cd1b20b855ae4412015
Guilherme <guilhermeggr23@gmail.com>
2025-06-16 20:31:25 -0300

    feat: adiciona o ngClass na imagem da coruja

--- src/app/mascote/mascote.component.ts
+++ src/app/mascote/mascote.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mascote',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mascote.component.html',
   styleUrl: './mascote.component.scss'
 })
