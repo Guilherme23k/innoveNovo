6f8641860954bd84a96a9887bda21a0ed235ebbb
Guilherme <guilhermeggr23@gmail.com>
2025-06-16 20:38:04 -0300

    feat: adiciona mais frases a coruja

--- src/app/mascote/mascote.component.ts
+++ src/app/mascote/mascote.component.ts
@@ -12,11 +12,23 @@ export class MascoteComponent {
 
   falas:string[] = [
     'Oi! Tudo bem?',
-    'Clica de novo pra eu falar mais!',
-    'Sabia que eu adoro eventos?',
-    'Corujas são sábias, igual eu 😎',
-    'Quer ver mais? É só continuar rolando!',
-    'Você é demais! 🦉✨'
+  'Clica de novo pra eu falar mais!',
+  'Sabia que eu adoro eventos?',
+  'Corujas são sábias, igual eu 😎',
+  'Quer ver mais? É só continuar rolando!',
+  'Você é demais! 🦉✨',
+  'Já tomou água hoje?',
+  'Se precisar de ajuda, estarei por aqui!',
+  'Tenho olhos de águia… ops, de coruja!',
+  'Sabia que trabalhar com eventos é uma arte?',
+  'Foco, força e… penas!',
+  'Minha missão? Ajudar você sempre que puder!',
+  'Sábias decisões começam com boas conversas!',
+  'Curtiu meu visual? Fui feito com carinho!',
+  'Você tá voando! Continue assim! 🕊️',
+  'Eu vi o futuro… e ele é incrível pra você!',
+  'O mundo precisa de mais ideias como as suas.',
+  'Hoje é um ótimo dia pra aprender algo novo!'
   ]
 
   falaAtual = this.falas[0];
